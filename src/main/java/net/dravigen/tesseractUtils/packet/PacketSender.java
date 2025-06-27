37610c9276aa043e9f209064e9467e9184bd1111
DraViGen <fireshadoxe@gmail.com>
2025-06-27 13:28:42 +0200

    v0.8.0-beta changelog:     - added the TPS, MSPT and mobcaps to the extra debug info     - reorganization of the configs structure     - added music keybind to start and stop music     - improved hotbar swap to work in multiplayer and swap column or whole inventory depending if you're crouching     - added the durability tooltips when extra debug info and advanced item tooltips are enable     - fixed shovel not taking durability when breaking blocks     - improved the /playsound to be easier to use     - added a way to make mobs permanent     - added the mob infos to the extra debug info     - removed the entity eraser to replace it with wooden sword

--- src/main/java/net/dravigen/tesseractUtils/packet/PacketSender.java
+++ src/main/java/net/dravigen/tesseractUtils/packet/PacketSender.java
@@ -4,9 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-import net.minecraft.src.EntityPlayerMP; // Server-side player
+import net.minecraft.src.EntityPlayerMP;
 import net.minecraft.src.Minecraft;
-import net.minecraft.src.Packet250CustomPayload; // The custom packet class
+import net.minecraft.src.Packet250CustomPayload;
 
 import net.dravigen.tesseractUtils.TesseractUtilsAddon.*;
 
@@ -19,7 +19,6 @@ public class PacketSender {
      * @param message The string message to send.
      */
     public static void sendClientToServerMessage(String message) {
-        // Ensure this is client-side code
         if (!Minecraft.getMinecraft().theWorld.isRemote) {
             System.err.println("Attempted to send C2S packet from server-side!");
             return;
@@ -29,11 +28,10 @@ public class PacketSender {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             DataOutputStream dos = new DataOutputStream(bos);
 
-            dos.writeUTF(message); // Write the string message
+            dos.writeUTF(message);
 
             Packet250CustomPayload packet = new Packet250CustomPayload(TUChannels.CLIENT_TO_SERVER_CHANNEL, bos.toByteArray());
 
-            // Add the packet to the client's send queue
             Minecraft.getMinecraft().getNetHandler().addToSendQueue(packet);
             //System.out.println("CLIENT: Sent C2S message: " + message);
 
@@ -48,10 +46,9 @@ public class PacketSender {
      * This method MUST be called on the SERVER side.
      *
      * @param player The player to send the message to.
-     * @param message The string message to send.
+     * @param message The string/boolean message to send.
      */
     public static void sendServerToClientMessage(EntityPlayerMP player, boolean message) {
-        // Ensure this is server-side code
         if (player.worldObj.isRemote) {
             System.err.println("Attempted to send S2C packet from client-side!");
             return;
@@ -61,10 +58,9 @@ public class PacketSender {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             DataOutputStream dos = new DataOutputStream(bos);
 
-            dos.writeUTF(String.valueOf(message)); // Write the string message
+            dos.writeUTF(String.valueOf(message));
 
             Packet250CustomPayload packet = new Packet250CustomPayload(TUChannels.SERVER_TO_CLIENT_CHANNEL, bos.toByteArray());
-            // Send the packet to the specified player
             player.playerNetServerHandler.sendPacketToPlayer(packet);
             //System.out.println("SERVER: Sent S2C message to " + player.getCommandSenderName() + ": " + message);
 
